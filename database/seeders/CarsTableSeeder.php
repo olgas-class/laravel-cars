c4cffd8ab3e740e87edcf09b9a5e1559667a16ad
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-13 12:38:33 +0200

    feat: model

--- database/seeders/CarsTableSeeder.php
+++ database/seeders/CarsTableSeeder.php
@@ -4,14 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CarsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i = 0; $i < 10; $i++) {
+            
+        }
     }
 }
